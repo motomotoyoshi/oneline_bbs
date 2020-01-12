a1fee821e4a0f610f0e0f0e133d7b968bcfb1a83
motomotoyoshi <motomoto.git@gmail.com>
2020-01-13 03:42:03 +0900

    nameとcommentをDBに保存する処理を追加

---
+++ bbs.php
@@ -0,0 +1,68 @@
+<?php
+require_once(__DIR__ . '/config.php');
+// DBに接続
+$link = mysqli_connect('localhost', DB_USERNAME, DB_PASSWORD);
+if (!$link) {
+    die('DBに接続できません: ' . mysqli_error($link));
+}
+// DBを選択
+mysqli_select_db($link, DB_NAME);
+$errors = array();
+
+// POSTなら保存処理実行
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = null;
+    if(!isset($_POST['name']) || !strlen($_POST['name'])) {
+        $errors['name'] = '名前を入力してください';
+    } else if (strlen($_POST['name']) > 40) {
+        $errors['name'] = '名前は40文字以内で入力してください';
+    } else {
+        $name = $_POST['name'];
+    }
+}
+
+// ひとことが正しく入力されているかチェック
+$comment = null;
+if (!isset($_POST['comment']) || !strlen($_POST['comment'])) {
+    $errors['comment'] = 'コメントを入力してください';
+} else if (strlen($_POST['comment']) > 200) {
+    $errors{'comment'} = 'コメントは200文字以内で入力してください';
+} else {
+    $comment = $_POST['comment'];
+}
+
+// エラーがなければ保存
+if (count($errors) === 0) {
+    $sql = "INSERT INTO post (name, comment, created_at) VALUES ('". mysqli_real_escape_string($link, $name). "', '". mysqli_real_escape_string($link, $comment) ."', '".date('Y-m-d H:i:s') . "')";
+
+    // printf($sql);
+    mysqli_query($link, $sql);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ひとこと掲示板</title>
+</head>
+<body>
+    <h1>ひとこと掲示板</h1>
+    <form action="bbs.php" method="post">
+        <?php if (count($errors)): ?>
+            <ul class="error_list">
+                <?php foreach ($errors as $error): ?>
+                    <li>
+                        <?php echo htmlspecialchars($error, ENT_QUOTES, 'utf-8')?>
+                    </li>
+                <?php endforeach ?>
+            </ul>
+        <?php endif;?>
+        name: <input type="text" name="name" id=""><br />
+        comment: <input type="text" name="comment" id=""><br />
+        <input type="submit" name="submit" value="submit">
+    </form>
+</body>
+</html>
